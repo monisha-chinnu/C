eb2cb545a0bfb6462a3af0c5f27204a91154944d
Monisha A <118587670+monisha-chinnu@users.noreply.github.com>
2022-11-20 16:17:48 +0530

    Add files via upload
    *
    **
    ***
    ****

---
+++ pattern3.c
@@ -0,0 +1,26 @@
+/*
+ ============================================================================
+ Name        : pattern.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	setbuf(stdout,NULL);
+	int i,j;
+	for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return EXIT_SUCCESS;
+}
