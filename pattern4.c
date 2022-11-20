edfcab5fcc3127ec33c32d048c719777f1156e12
Monisha A <118587670+monisha-chinnu@users.noreply.github.com>
2022-11-20 16:28:04 +0530

    Add files via upload
    ****
    ***
    **
    *

---
+++ pattern4.c
@@ -0,0 +1,26 @@
+/*
+ ============================================================================
+ Name        : pattern4.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	setbuf(stdout,NULL);
+	int i,j;
+	for(i=1;i<=4;i++)
+	{
+		for(j=4;j>=i;j--)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return EXIT_SUCCESS;
+}
